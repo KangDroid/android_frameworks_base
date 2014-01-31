2bb8528ff3c3745abd6cda3f432c333b0155b0f4
David96 <xd_e-mail@gmx.de>
2014-02-01 00:12:45 +0100

    Lockscreen Notifications: fix show/hide of show all button
    & fix endless SystemUI FC when SystemUI is killed
    
    Change-Id: Ie46ef525f72b7ce575dacfb1f05e46a140c87cc5
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -106,6 +106,7 @@ public class NavigationBarView extends LinearLayout {
     boolean mShowMenu;
     int mDisabledFlags = 0;
     int mNavigationIconHints = 0;
+    boolean mWasNotifsButtonVisible = false;
 
     private Drawable mBackIcon, mBackLandIcon, mBackAltIcon, mBackAltLandIcon;
     private Drawable mRecentIcon;
@@ -660,7 +661,8 @@ public class NavigationBarView extends LinearLayout {
             public void run() {
                 if (iconId == 1) iv.setImageResource(R.drawable.search_light_land);
                 else iv.setImageDrawable(mVertical ? mRecentAltLandIcon : mRecentAltIcon);
-                setVisibleOrGone(getNotifsButton(), iconId != 0);
+                mWasNotifsButtonVisible = iconId != 0 && ((mDisabledFlags & View.STATUS_BAR_DISABLE_HOME) != 0);
+                setVisibleOrGone(getNotifsButton(), mWasNotifsButtonVisible);
             }
         });
     }
@@ -725,8 +727,7 @@ public class NavigationBarView extends LinearLayout {
                 && NavigationRingHelpers.hasLockscreenTargets(mContext));
         setVisibleOrGone(getCameraButton(), showCamera);
         setVisibleOrGone(getApplicationWidgetButton(), showApplicationWidget);
-	if (!showNotifs) setVisibleOrGone(getNotifsButton(), showNotifs);
-
+	setVisibleOrGone(getNotifsButton(), showNotifs && mWasNotifsButtonVisible);
         mBarTransitions.applyBackButtonQuiescentAlpha(mBarTransitions.getMode(), true /*animate*/);
     }
 
