309d5f049c8ba2b7bbfd6ddd6396d80f08f4a580
Dave Kessler AlmightyMegadeth00 <activethrasher00@gmail.com>
2014-02-04 15:53:28 -0700

    FWB - Add PowerMenu QS Tile (Reboot on longpress) [1/2]
    Porting from KK to LP
    
    PS2: whiten icons and add blur
    
    Change-Id: I6e44b91ce14b3c732c990624fba381b8b257f859
    Signed-off-by: STELIX <ssspinni@gmail.com>
    
    Conflicts:
    
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5738,6 +5738,12 @@ public class WindowManagerService extends IWindowManager.Stub
 
     // Called by window manager policy.  Not exposed externally.
     @Override
+    public void rebootTile() {
+        ShutdownThread.reboot(mContext, null, true);
+    }
+
+    // Called by window manager policy.  Not exposed externally.
+    @Override
     public void rebootSafeMode(boolean confirm) {
         ShutdownThread.rebootSafeMode(mContext, confirm);
     }
