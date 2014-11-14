9733918a084f1d3e33c2ba442bd21cb722df9b93
Roman Birg <roman@cyngn.com>
2014-11-14 09:44:50 -0800

    GlobalActions: public CharSequence getLabelForAccessibility
    Change-Id: Ia600163654c9486a09e0cc632d6c8a4e964edda0

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -362,6 +362,11 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 public boolean showBeforeProvisioning() {
                     return false;
                 }
+
+                public CharSequence getLabelForAccessibility(Context context) {
+                    return null;
+                }
+
             });
 
         mAdapter = new MyAdapter();
