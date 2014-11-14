bb253494166a92fdda5c8d2ce0db61efeb8420ca
Raj Yengisetty <rajesh@cyngn.com>
2014-11-14 09:44:50 -0800

    Protected apps
    Change-Id: Ic690387cd21fdfa09ef5fb19bd3de9305050cf6e

--- core/java/android/content/pm/ApplicationInfo.java
+++ core/java/android/content/pm/ApplicationInfo.java
@@ -587,6 +587,13 @@ public class ApplicationInfo extends PackageItemInfo implements Parcelable {
      * @hide
      */
     public int installLocation = PackageInfo.INSTALL_LOCATION_UNSPECIFIED;
+	
+   /**
+    * When true, indicates that any one component within this application is
+    * protected.
+    * @hide
+    */
+   public boolean protect = false;
 
     /**
      * Is given application theme agnostic, i.e. behaves properly when default theme is changed.
@@ -717,6 +724,7 @@ public class ApplicationInfo extends PackageItemInfo implements Parcelable {
         uiOptions = orig.uiOptions;
         backupAgentName = orig.backupAgentName;
         isThemeable = orig.isThemeable;
+        protect = orig.protect;
     }
 
 
@@ -768,6 +776,7 @@ public class ApplicationInfo extends PackageItemInfo implements Parcelable {
         dest.writeInt(descriptionRes);
         dest.writeInt(uiOptions);
         dest.writeInt(isThemeable ? 1 : 0);
+        dest.writeInt(protect ? 1 : 0);
     }
 
     public static final Parcelable.Creator<ApplicationInfo> CREATOR
@@ -818,6 +827,7 @@ public class ApplicationInfo extends PackageItemInfo implements Parcelable {
         descriptionRes = source.readInt();
         uiOptions = source.readInt();
         isThemeable = source.readInt() != 0;
+        protect = source.readInt() != 0;
     }
 
     /**
--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5922,6 +5922,11 @@ public final class Settings {
          */
         public static final String SLEEP_TIMEOUT = "sleep_timeout";
 
+         /** Protected Components
+         * @hide
+         */
+        public static final String PROTECTED_COMPONENTS = "protected_components";
+
         /**
          * Default theme to use.  If empty, use holo.
          * @hide
