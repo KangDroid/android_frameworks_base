26538f319b0c3648007ff6e58772f90ffb0f40f4
Alex Cruz <mazdarider23@gmail.com>
2014-12-06 17:19:23 +0100

    FWB: Network traffic supercharged (1/2)
    Bring back AICP kk traffic
    
    Change-Id: Ide3ac899da7632dd1fb3a0615ab9560a4ae6cfd0
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3200,20 +3200,32 @@ public final class Settings {
         public static final String VOICE_LAUNCH_INTENT = "voice_launch_intent";
 
        /**
-         * Network traffic indicator, goes from least to greatest significant bitwise
-         * 0 = Display up-stream traffic if set
-         * 1 = Display down-stream traffic if set
-         * 2 = Show as Byte/s if set
-         * 16-31 = Refresh interval(ms) min: 250 max: 32750 default: 1000
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+        * Network traffic indicator, goes from least to greatest significant bitwise
+        * 0 = Display up-stream traffic if set
+        * 1 = Display down-stream traffic if set
+        * 2 = Show as Byte/s if set
+        * 16-31 = Refresh interval(ms) min: 250 max: 32750 default: 1000
+        * @hide
+        */
+       public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
-        /**
-         * Whether or not to hide the network traffic indicator when there is no activity
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_AUTOHIDE = "network_traffic_autohide";
+       /**
+        * Network stats Color style
+        * @hide
+        */
+       public static final String NETWORK_TRAFFIC_COLOR = "network_traffic_color";
+
+       /**
+        * Whether or not to hide the network traffic indicator when there is no activity
+        * @hide
+        */
+       public static final String NETWORK_TRAFFIC_AUTOHIDE = "network_traffic_autohide";
+
+       /**
+        * Network traffic inactivity threshold (default is 10 kBs)
+        * @hide
+        */
+       public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
          /**
          * Recents Show/Hide Search Bar
@@ -3226,6 +3238,7 @@ public final class Settings {
          * @hide
          */
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
 
        /**
          * Show or hide clock
