d417b19af0fc55d1268f1b0ed8c09d4b85cba1a7
0xD34D <clark@scheffsblend.com>
2014-12-14 21:23:05 +0100

    FWB: Gesture Anywhere - bring it back [1/2]
    AICPfy lp5.0:
    - Lollipopify LOL
    - By request from doc
    - Added TriggerOverlayView from AppSidebar
    - some lines from Locskcreen gestures to get
      it compiled and working
    
    Change-Id: I04acb9a88fe0011921a1bafdc1919d76ce57aedf
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -35,6 +35,8 @@ import static com.android.systemui.statusbar.phone.BarTransitions.MODE_WARNING;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.TimeInterpolator;
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
 import android.annotation.NonNull;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
@@ -782,6 +784,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // ================================================================================
     // Constructing the view
     // ================================================================================
+    @ChaosLab(name="GestureAnywhere", classification=Classification.CHANGE_CODE)
     protected PhoneStatusBarView makeStatusBarView() {
         final Context context = mContext;
 
@@ -854,6 +857,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         updateShowSearchHoldoff();
+		addGestureAnywhereView();
 
         try {
             boolean showNav = mWindowManagerService.hasNavigationBar();
@@ -883,6 +887,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
         } catch (RemoteException ex) {
             // no window manager? good luck with that
+
         }
 
         RecentsActivity.setNavigationBarView(mNavigationBarView);
