9798d32f0a4b6f71fee44d38682075118dbb184e
KangDroid <KangDroid@KangDroidui-MacBook-Air.local>
2014-12-31 16:03:53 +0000

    Revert "Revert "[1/2] Screen Recorder by 0xD34D""
    This reverts commit 6a1aa1adb34b0fb4df77ae7224cea67ffb579cf7.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3376,7 +3376,30 @@ public final class Settings {
          * @hide
          */
         public static final String RECENTS_SHOW_HIDE_SEARCH_BAR = "recents_show_hide_search_bar";
+		 
+         /**
+          * Width and height of output vide expressed in WxH
+          * @hide
+          */
+         public static final String SCREEN_RECORDER_OUTPUT_DIMENSIONS = "screen_recorder_output_dimensions";
 
+         /**
+          * Screen recorder framerate in bits per second
+          * @hide
+          */
+         public static final String SCREEN_RECORDER_BITRATE = "screen_recorder_bitrate";
+
+         /**
+          * Whether power menu screen record is enabled
+          * @hide
+          */
+         public static final String POWER_MENU_SCREENRECORD_ENABLED = "power_menu_screenrecord_enabled";
+
+         /**
+          * Whether to include audio when recording a video
+          * @hide
+          */
+         public static final String SCREEN_RECORDER_RECORD_AUDIO = "screen_recorder_record_audio";
          /**
           * Settings for clock font style
           * @hide
