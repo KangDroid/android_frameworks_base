9633a9ec62b593f82f0b91552dc18dc227f4b818
KangDroid <KangDroid@KangDroidui-MacBook-Air.local>
2015-01-07 23:05:27 +0000

    Revert "Framework: Add Fade in and Fade out effect to Recents FAB"
    This reverts commit b38850480c0059b96357fd8f74cea9bb1a880fd7.

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -662,8 +662,6 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
 
         // Dismiss Recents to the focused Task or Home
         dismissRecentsToFocusedTaskOrHome(true);
-
-        mRecentsView.endFABanimation();
     }
 
     /** Called when debug mode is triggered */
@@ -694,7 +692,6 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
     public void onEnterAnimationTriggered() {
         // Animate the SystemUI scrim views
         mScrimViews.startEnterRecentsAnimation();
-        mRecentsView.startFABanimation();
     }
 
     /**** RecentsView.RecentsViewCallbacks Implementation ****/
@@ -703,7 +700,6 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
     public void onExitToHomeAnimationTriggered() {
         // Animate the SystemUI scrim views out
         mScrimViews.startExitRecentsAnimation();
-        mRecentsView.endFABanimation();
     }
 
     @Override
@@ -711,20 +707,17 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
         // Mark recents as no longer visible
         AlternateRecentsComponent.notifyVisibilityChanged(false);
         mVisible = false;
-        mRecentsView.endFABanimation();
     }
 
     @Override
     public void onTaskLaunchFailed() {
         // Return to Home
         dismissRecentsToHomeRaw(true);
-        mRecentsView.endFABanimation();
     }
 
     @Override
     public void onAllTaskViewsDismissed() {
         mFinishLaunchHomeRunnable.run();
-        mRecentsView.endFABanimation();
     }
 
     /**** RecentsAppWidgetHost.RecentsAppWidgetHostCallbacks Implementation ****/
