fe6378437631994c0f5059207e1511c9ae035e2d
maxwen <max.weninger@gmail.com>
2015-01-16 01:01:53 +0100

    base: allow forcing full screen alarm notification
    Used from DeskClock app to force showing the full screen intent
    instead of a heads-up notification on demand
    
    Change-Id: I7764ed0526fe1c0d11bd2e6dbf6f6eef2cabe71b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4132,6 +4132,13 @@ public final class Settings {
                 "pie_buttons_config_second_layer";
 
         /**
+         * Should the alarm be shown in fullscreen instead of heads-up
+         *
+         * @hide
+         */
+        public static final String SHOW_ALARM_FULLSCREEN = "show_alarm_fullscreen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
