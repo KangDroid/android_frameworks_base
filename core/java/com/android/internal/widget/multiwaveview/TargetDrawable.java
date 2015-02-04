dd2063ea327480a7e6ce431661436579cd5fbef6
KangDroid <kangdroid@naver.com>
2015-02-04 18:37:45 +0000

    Revert "NavTargets : Show highlight for google search"
    This reverts commit 67d690a5b60775e1e42885f4cee391bcc1bc13ad.

--- core/java/com/android/internal/widget/multiwaveview/TargetDrawable.java
+++ core/java/com/android/internal/widget/multiwaveview/TargetDrawable.java
@@ -62,12 +62,6 @@ public class TargetDrawable {
         setState(STATE_INACTIVE);
     }
 
-    public void setDrawable(Drawable drawable) {
-        mDrawable = drawable != null ? drawable.mutate() : null;
-        resizeDrawables();
-        setState(STATE_INACTIVE);
-    }
-
     public TargetDrawable(Resources res, Drawable drawable) {
         mResourceId = 0;
         // Mutate the drawable so we can animate shared drawable properties.
@@ -220,10 +214,6 @@ public class TargetDrawable {
 
     public int getHeight() {
         return mDrawable != null ? mDrawable.getIntrinsicHeight() : 0;
-    }
-
-    public Drawable getDrawable() {
-        return mDrawable;
     }
 
     public void draw(Canvas canvas) {
