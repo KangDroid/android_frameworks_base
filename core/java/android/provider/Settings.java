a8e6929683ba52ed114d05f9bedf0a08ae875621
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2015-02-08 01:16:00 +0700

    Implement ambient display as Active Display
    Currently Ambient Display fully support if device has
      Significant motion sensor and wake gesture sensor
    
    with this change allow Ambient Display to work like
        Active Display
    
    -- Allow wake up using shake event
    -- use different class to manage sensor
    
    enable this via overlay custom_config.xml
    
    inside Frameworks base
    
        <!-- Doze: force using accelerometer as pick up sensor -->
        <bool name="config_dozeUseAccelerometer">false</bool>
    
    inside System UI
    
        <!-- Doze: shake accelerometer threshold -->
        <integer name="doze_shake_accelerometer_threshold">10</integer>
    
    also dont forget to setup other value for Ambient Display
    
    see https://gerrit.omnirom.org/10140
    
    PS. 12 : prepare for user configurations
    
    NOTE:
       Shake event aka Wake up will not triggered until
       Proximity is Covered (need to put inside pocket)
    
       for shake accelerometer, you must setup based on your device
       each device has different threshold
    
    NOTE 2 :
    
       you can also overwrite all value for Ambient Display inside BUILD.PROP
       but i recommend only overwrite value from overlay fw base and system Ui
    
       example value inside BUILD.PROP :
    
       <!-- Doze: does this device support STATE_DOZE and STATE_DOZE_SUSPEND?  -->
       doze.display.supported=true
    
       <!-- Doze: pulse parameter - how long does it take to fade in? -->
       doze.pulse.duration.in=1000
    
       <!-- Doze: pulse parameter - once faded in, how long does it stay visible? -->
       doze.pulse.duration.visible=3000
    
       <!-- Doze: pulse parameter - how long does it take to fade out? -->
       doze.pulse.duration.out=1000
    
       <!-- Doze: should notifications be used as a pulse signal? -->
       doze.pulse.notifications=true
    
       <!-- Doze: when to pulse after a buzzworthy notification arrives -->
       doze.pulse.schedule=1s,10s,30s,60s,120s
    
       <!-- Doze: maximum number of times the notification pulse schedule can be reset -->
       doze.pulse.schedule.resets=3
    
       <!-- Doze: shake accelerometer threshold -->
       doze.shake.acc.threshold=10
    
       <!-- Doze: force using accelerometer as pick up sensor -->
       doze.use.accelerometer=true
    
    Feature describes:
        1. Full mode (pocket,shake,pulse)
        2. Pocket and shake only
        3. Pocket and pulse only
        4. Pocket only
        5. Shake and pulse only
        6. Pulse only (default)
    
    Note :
        Shake mode only enabled after Doze is triggered by proximity or notifications (depends on option)
        so if doze not triggered by that, shake mode will stay disabled.
    
    Change-Id: I6e1d634c9c8b1c3d3ac938c572d9f79902e6e9e5
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/custom_config.xml
    	core/res/res/values/custom_symbols.xml
    	packages/SystemUI/Android.mk

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2352,6 +2352,54 @@ public final class Settings {
         @Deprecated
         public static final String QS_USE_MAIN_TILES = "sysui_qs_main_tiles";
 
+		/**
+         * Enable/disable overwrite default value for ambient display
+         * @hide
+         */
+        public static final String DOZE_OVERWRITE_VALUE = "doze_overwrite_value";
+
+        /**
+         * Duration in for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_DURATION_IN = "doze_pulse_duration_in";
+
+        /**
+         * Duration visible for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_DURATION_VISIBLE = "doze_pulse_duration_visible";
+
+        /**
+         * Duration out for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_DURATION_OUT = "doze_pulse_duration_out";
+
+        /**
+         * Pulse if there is notifications for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_ON_NOTIFICATIONS = "doze_pulse_on_notifications";
+
+        /**
+         * Shake threshold for ambient display
+         * @hide
+         */
+        public static final String DOZE_SHAKE_ACC_THRESHOLD = "doze_shake_acc_threshold";
+
+        /**
+         * Pocket mode for ambient display
+         * @hide
+         */
+        public static final String DOZE_POCKET_MODE = "doze_pocket_mode";
+
+        /**
+         * Pocket mode for ambient display
+         * @hide
+         */
+        public static final String DOZE_SHAKE_MODE = "doze_shake_mode";
+
         /**
          * Control whether the process CPU usage meter should be shown.
          *
