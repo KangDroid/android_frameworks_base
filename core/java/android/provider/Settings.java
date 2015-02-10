cb68acc756a849c40e57b6e84c6143d5f43afef8
KangDroid <kangdroid@naver.com>
2015-02-10 21:36:11 +0000

    Revert "Revert ScreenRecord [SQUASHED]"
    This reverts commit cb39f56439ff428b2f40146ad6e2201961645af7.
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3637,7 +3637,31 @@ public final class Settings {
          * @hide
          */
         public static final String VOICE_LAUNCH_INTENT = "voice_launch_intent";
+		 
+         /**
+          * Width and height of output vide expressed in WxH
+          * @hide
+          */
+         public static final String SCREEN_RECORDER_OUTPUT_DIMENSIONS = "screen_recorder_output_dimensions";
 
+         /**
+          * Screen recorder framerate in bits per second
+          * @hide
+          */
+         public static final String SCREEN_RECORDER_BITRATE = "screen_recorder_bitrate";
+
+         /**
+          * Whether power menu screen record is enabled
+          * @hide
+          */
+         public static final String POWER_MENU_SCREENRECORD_ENABLED = "power_menu_screenrecord_enabled";
+
+         /**
+          * Whether to include audio when recording a video
+          * @hide
+          */
+         public static final String SCREEN_RECORDER_RECORD_AUDIO = "screen_recorder_record_audio";
+		 
          /**
           * Settings for clock font style
           * @hide
--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -43,6 +43,7 @@ import android.content.pm.UserInfo;
 import android.content.ServiceConnection;
 import android.database.ContentObserver;
 import android.graphics.drawable.Drawable;
+import android.Manifest;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.os.Build;
@@ -126,6 +127,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     private boolean mHasTelephony;
     private boolean mHasVibrator;
     private final boolean mShowSilentToggle;
+    private final boolean mShowScreenRecord;
     private Profile mChosenProfile;
 
     // Power menu customizations
@@ -168,6 +170,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         mShowSilentToggle = SHOW_SILENT_TOGGLE && !mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_useFixedVolume);
+        mShowScreenRecord = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_enableScreenrecordChord);
 
         updatePowerMenuActions();
     }
@@ -284,6 +288,37 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         onAirplaneModeChanged();
 
         mItems = new ArrayList<Action>();
+
+        // next: screen record, if enabled
+        if (mShowScreenRecord) {
+            try {
+                if (Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.POWER_MENU_SCREENRECORD_ENABLED, 0) != 0) {
+                    mItems.add(
+                        new SinglePressAction(com.android.internal.R.drawable.ic_lock_screen_record,
+                                R.string.global_action_screen_record) {
+
+                            public void onPress() {
+                                toggleScreenRecord();
+                            }
+
+                            public boolean onLongPress() {
+                                return false;
+                            }
+
+                            public boolean showDuringKeyguard() {
+                                return true;
+                            }
+
+                            public boolean showBeforeProvisioning() {
+                                return true;
+                            }
+                        });
+                }
+            } catch (NullPointerException e) {
+              // Do nothing
+            }
+        }
 
         String[] actionsArray;
         if (mActions == null) {
@@ -700,6 +735,11 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 }
             }
         }
+    }
+
+    private void toggleScreenRecord() {
+        final Intent recordIntent = new Intent("org.chameleonos.action.NOTIFY_RECORD_SERVICE");
+        mContext.sendBroadcast(recordIntent, Manifest.permission.RECORD_SCREEN);
     }
 
     /**
