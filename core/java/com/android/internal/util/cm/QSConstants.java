af74f20cebb29f05351164c69585cd533f867f81
Alex Cruz <mazdarider23@gmail.com>
2015-02-20 06:08:14 +0000

    NavBar tile [1/3]
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Change-Id: Ib60e3ef587397eb9ee8bb2dd212dbd1636b9045f
    
    Conflicts:
    	packages/SystemUI/res/values/strings.xml

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -51,6 +51,7 @@ public class QSConstants {
     public static final String TILE_BRIGHTNESS = "brightness";
     public static final String TILE_BATTERY_SAVER = "battery_saver";
     public static final String TILE_EXPANDED_DESKTOP = "expanded_desktop";
+    public static final String TILE_NAVBAR = "toggleNavBar";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -92,5 +93,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_BRIGHTNESS);
         TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
         TILES_AVAILABLE.add(TILE_EXPANDED_DESKTOP);
+        TILES_AVAILABLE.add(TILE_NAVBAR);
     }
 }
