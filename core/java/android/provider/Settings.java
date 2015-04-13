85d8dedd21a9be264feac677a239a9737c438f4e
KangDroid <hyunwoo286@gmail.com>
2015-04-13 22:38:22 +0100

    Second Clock and Configurable Color
    Allows you to customize Color and "Second" Clock in CM's Clock customization.
    
    WISHLIST:
    Enabler Switch for whether we hide clock or show.
    
    Change-Id: Ic8e29a594a4dbda2f0669dea5779ddf9cac1782b
    Signed-off-by: KangDroid <hyunwoo286@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3598,6 +3598,18 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+		
+		/**
+		 * Setting for clock color
+		 * @hide
+		 */
+        public static final String STATUSBAR_CLOCK_COLOR = "statusbar_clock_color";
+		
+        /**
+         * Display second in the Clock
+         * @hide
+         */
+        public static final String CLOCK_USE_SECOND = "clock_use_second";  
 
         /**
          * Shows custom date before clock time
