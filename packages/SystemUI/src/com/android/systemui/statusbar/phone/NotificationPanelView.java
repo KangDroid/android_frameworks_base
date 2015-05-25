46162f8b6d589baa06cc794116300139b483be01
Susan McCombs <smac0628@gmail.com>
2015-05-25 14:59:01 +0200

    Fix ForceClose due to TaskManger.
    Change-Id: I665a366abe857c51dbb111a57048153527ab1007

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -366,6 +366,7 @@ public class NotificationPanelView extends PanelView implements
         updateHeader();
         mNotificationStackScroller.updateIsSmallScreen(
                 mHeader.getCollapsedHeight() + mQsPeekHeight);
+        requestPanelHeightUpdate();
     }
 
     @Override
