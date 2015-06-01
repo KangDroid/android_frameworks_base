7e7c065c8f8d6c7ee0d9eaf34a946824cee3e330
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-06-01 14:33:44 +0100

    SystemUI: Double tap to sleep anywhere on the lock screen (1/2)
    Change-Id: I848161bca30e58009ade21cbba3e5e1672ba85ac
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3751,6 +3751,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Enable double tap gesture anywhere on the lock screen put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_ANYWHERE = "double_tap_sleep_anywhere";
+
+        /**
           * Volume keys control cursor in text fields (default is 0)
           * 0 - Disabled
           * 1 - Volume up/down moves cursor left/right
