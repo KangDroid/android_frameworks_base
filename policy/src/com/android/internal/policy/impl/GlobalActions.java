fb125709e1a1978073d0ba959edcfe0138f476a1
KangDroid <hyunwoo286@gmail.com>
2015-07-25 18:11:26 +0100

    Revert "Fix NPE causing a hot reboot when pressing power button"
    This reverts commit 7dd1497a5f500b60745e944a2e8da566d46b450f.

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -1327,7 +1327,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
         @Override
         public void onServiceStateChanged(ServiceState serviceState) {
-            if (!mHasTelephony || mAirplaneModeOn == null || mAdapter == null) return;
+            if (!mHasTelephony) return;
             final boolean inAirplaneMode = serviceState.getState() == ServiceState.STATE_POWER_OFF;
             mAirplaneState = inAirplaneMode ? ToggleAction.State.On : ToggleAction.State.Off;
             mAirplaneModeOn.updateState(mAirplaneState);
