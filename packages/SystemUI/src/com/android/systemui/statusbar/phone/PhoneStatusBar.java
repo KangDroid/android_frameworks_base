b9886025d49686eaf3b2bf76837d687e5c069e89
LorDClockaN <davor@losinj.com>
2015-07-26 21:32:21 +0200

    FWB: Statusbar weather color (1/2)
    Change-Id: I20f3ce2ac5acdc823a3bb180efa0eeff1cac1b66

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -347,6 +347,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     TextView mWeatherTempView;
     private int mWeatherTempState;
     private int mWeatherTempStyle;
+    private int mWeatherTempColor;
 
     // the icons themselves
     IconMerger mNotificationIcons;
@@ -570,6 +571,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_WEATHER_COLOR),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -660,7 +664,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 		                    updateClearAll();
 		                    updateEmptyShadeView();	
             } else if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE))) {
+                Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE))
+                || uri.equals(Settings.System.getUriFor(
+                Settings.System.STATUS_BAR_WEATHER_COLOR))) {
 		                    recreateStatusBar();
 		                    updateRowStates();
 		                    updateSpeedbump();
@@ -755,12 +761,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     resolver, Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE, 0,
                     UserHandle.USER_CURRENT);
 
+            mWeatherTempColor = Settings.System.getIntForUser(resolver,
+                    Settings.System.STATUS_BAR_WEATHER_COLOR, 0xFFFFFFFF, mCurrentUserId);
+
             final int oldWeatherState = mWeatherTempState;
             mWeatherTempState = Settings.System.getIntForUser(
                     resolver, Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0,
                     UserHandle.USER_CURRENT);
             if (oldWeatherState != mWeatherTempState) {
-                updateWeatherTextState(mWeatherController.getWeatherInfo().temp);
+                updateWeatherTextState(mWeatherController.getWeatherInfo().temp, mWeatherTempColor);
             }
         }
     }
@@ -804,7 +813,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 || clockLocation == Clock.STYLE_CLOCK_LEFT;
     }
 
-    private void updateWeatherTextState(String temp) {
+    private void updateWeatherTextState(String temp, int color) {
         if (mWeatherTempState == 0 || TextUtils.isEmpty(temp)) {
             mWeatherTempView.setVisibility(View.GONE);
             return;
@@ -816,6 +825,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         } else if (mWeatherTempState == 2) {
             mWeatherTempView.setText(temp.substring(0, temp.length() - 1));
         }
+        mWeatherTempView.setTextColor(color);
         mWeatherTempView.setVisibility(View.VISIBLE);
     }
 
@@ -1429,6 +1439,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mWeatherTempView = (TextView) mStatusBarView.findViewById(R.id.left_weather_temp);
         }
 
+        mWeatherTempColor = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_WEATHER_COLOR, 0xFFFFFFFF, mCurrentUserId);
         mWeatherTempState = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0,
                 UserHandle.USER_CURRENT);
@@ -1437,11 +1449,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mWeatherController.addCallback(new WeatherController.Callback() {
                 @Override
                 public void onWeatherChanged(WeatherInfo temp) {
-                    updateWeatherTextState(temp.temp);
+                    updateWeatherTextState(temp.temp, mWeatherTempColor);
                 }
             });
         }
-        updateWeatherTextState(mWeatherController.getWeatherInfo().temp);
+        updateWeatherTextState(mWeatherController.getWeatherInfo().temp, mWeatherTempColor);
 
         mKeyguardBottomArea.setPhoneStatusBar(this);
         if (mAccessibilityController == null) {
