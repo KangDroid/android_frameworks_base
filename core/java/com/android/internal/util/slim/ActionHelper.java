6eda38b340905a8835a8e5acfa4ab032d4e006d3
SpiritCroc <spiritcroc@gmail.com>
2015-08-29 11:43:21 +0200

    [1/2] Slim Recent App Sidebar
    One thing I was missing with SlimRecents was the possibility to launch your favorite apps from the
    recents screen. As SlimRecents covers only the half of the screen (more or less), there is enough
    space on the other edge to display an app sidebar, which I based on the ChameleonOS one.
    
    Changes to the ChameleonOS sidebar include:
    - No trigger areas, but show/hide with the SlimRecent screen
    - Adapted look and animation to SlimRecent
    - Longpress actions
    - Slimer settings using ActionListViewSettings, also added possibility to configure sidebar size
      (roughly, as the size is adapted to the screen height in order to show full icons) and colors
    - Currently no option to add folders to the sidebar, as this does not work with the current
      ActionListViewSettings without further work
    
    PS 2 Changes:
    
    - Shorter lines
    
    Change-Id: I1f3edce2ed12297a7a7cb976f2a2315f86492192

--- core/java/com/android/internal/util/slim/ActionHelper.java
+++ core/java/com/android/internal/util/slim/ActionHelper.java
@@ -207,6 +207,40 @@ public class ActionHelper {
                     Settings.System.PIE_BUTTONS_CONFIG_SECOND_LAYER,
                     config);
     }
+	
+    public static ArrayList<ActionConfig> getRecentAppSidebarConfig(Context context) {
+        return (ConfigSplitHelper.getActionConfigValues(context,
+                getRecentAppSidebarProvider(context), null, null, false));
+    }
+
+    public static ArrayList<ActionConfig> getRecentAppSidebarConfigWithDescription(
+            Context context, String values, String entries) {
+        return (ConfigSplitHelper.getActionConfigValues(context,
+                getRecentAppSidebarProvider(context), values, entries, false));
+    }
+
+    private static String getRecentAppSidebarProvider(Context context) {
+        String config = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.RECENT_APP_SIDEBAR_CONTENT,
+                UserHandle.USER_CURRENT);
+        if (config == null) {
+            config = "";
+        }
+        return config;
+    }
+
+    public static void setRecentAppSidebarConfig(
+            Context context, ArrayList<ActionConfig> actionConfig, boolean reset) {
+        String config;
+        if (reset) {
+            config = "";
+        } else {
+            config = ConfigSplitHelper.setActionConfig(actionConfig, false);
+        }
+        Settings.System.putString(context.getContentResolver(),
+                Settings.System.RECENT_APP_SIDEBAR_CONTENT, config);
+    }
 
     private static int getActionSystemIcon(Resources systemUiResources, String clickAction) {
         int resId = -1;
